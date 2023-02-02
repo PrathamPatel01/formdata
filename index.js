bc16e2cb8cfea54e87d542a07a49631e2394be8b
PrathamPatel01 <90601027+PrathamPatel01@users.noreply.github.com>
2023-02-02 11:16:25 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const express = require("express");
+// const mysql = require("mysql");
 
 const bodyParser = require("body-parser");
 const app = express();
 const usersRepo = require("./repositories/users");
+const connection = require("./database");
+const db = require("./database");
 
 app.use(bodyParser.urlencoded({ extended: true }));
 const {
@@ -12,11 +15,21 @@ const {
   requireComments,
 } = require("./validators");
 
+//--------------------------Brand Form  data--------------------------------------------
+
 app.get(
   "/",
   [requireUsername, requireEmail, requireContact, requireComments],
   (req, res) => {
     const { username, email, contact, comments } = req.body;
+
+    //to display data on browser
+    // let sql = "SELECT * FROM user_info";
+    // connection.query(sql, function (err, results) {
+    //   if (err) throw err;
+    //   res.send(results);
+    // });
+
     res.send(`
     <div>
     <form method="POST">
@@ -33,11 +46,28 @@ app.get(
 
 app.post("/", async (req, res) => {
   const { username, email, contact, comments } = req.body;
-  const user = await usersRepo.create({ username, email, contact, comments });
+  
+  //to insert data into json file
+  // const user = await usersRepo.create({ username, email, contact, comments });
 
-  res.send("submitted");
+  const sql = `INSERT INTO user_info (user_name, email, contact, comments) VALUES ("${username}", "${email}", "${contact}", "${comments}")`;
+  connection.query(sql, function (err, result) {
+    if (err) throw err;
+    console.log("record inserted");
+    // req.flash("success", "Data added successfully!");
+    // res.redirect("/");
+  });
+
+  res.send("Submitted");
 });
+//------------------------------------------------------------------------------------------------
 
 app.listen(3000, () => {
-  console.log("Running");
+  console.log("Running on PORT3K");
+  connection.connect(function (err) {
+    if (err) throw err;
+    console.log("database connected");
+  });
 });
+
+module.exports = connection;
